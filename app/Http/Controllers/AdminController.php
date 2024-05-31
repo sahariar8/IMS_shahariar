69dbafeb1514649b4ba9acd092c6c8ba7ad37d1d
shahariar <shahariar@inneed.cloud>
2024-05-31 15:14:32 +0600

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
